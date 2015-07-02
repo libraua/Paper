e6e6e67c3108782d048a315040040204bf8b8389
Alex K <libraua@gmail.com>
2015-07-02 12:27:55 +0200

    Removing todo since PaperDbException is already throws Allow saving of null object instead of deleting key. Refactoring. From Singleton to Static Factory Allow saving of null object instead of deleting key. Revert "Allow saving of null object instead of deleting key."

--- paperdb/src/androidTest/java/io/paperdb/DataTest.java
+++ paperdb/src/androidTest/java/io/paperdb/DataTest.java
@@ -23,47 +23,47 @@ import static io.paperdb.testdata.TestDataGenerator.genPersonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests List write/read API
+ * Tests List put/get API
  */
 @RunWith(AndroidJUnit4.class)
 public class DataTest {
 
     @Before
     public void setUp() throws Exception {
-        Paper.init(getTargetContext());
+        Paper.clear(getTargetContext());
         Paper.book().destroy();
     }
 
     @Test
     public void testPutEmptyList() throws Exception {
         final List<Person> inserted = genPersonList(0);
-        Paper.book().write("persons", inserted);
-        assertThat(Paper.book().<List>read("persons")).isEmpty();
+        Paper.put("persons", inserted);
+        assertThat(Paper.<List>get("persons")).isEmpty();
     }
 
     @Test
     public void testPutGetList() {
         final List<Person> inserted = genPersonList(10000);
-        Paper.book().write("persons", inserted);
-        List<Person> persons = Paper.book().read("persons");
+        Paper.put("persons", inserted);
+        List<Person> persons = Paper.get("persons");
         assertThat(persons).isEqualTo(inserted);
     }
 
     @Test
     public void testPutMap() {
         final Map<Integer, Person> inserted = genPersonMap(10000);
-        Paper.book().write("persons", inserted);
+        Paper.put("persons", inserted);
 
-        final Map<Integer, Person> personMap = Paper.book().read("persons");
+        final Map<Integer, Person> personMap = Paper.get("persons");
         assertThat(personMap).isEqualTo(inserted);
     }
 
     @Test
     public void testPutPOJO() {
         final Person person = genPerson(1);
-        Paper.book().write("profile", person);
+        Paper.put("profile", person);
 
-        final Person savedPerson = Paper.book().read("profile");
+        final Person savedPerson = Paper.get("profile");
         assertThat(savedPerson).isEqualTo(person);
         assertThat(savedPerson).isNotSameAs(person);
     }
@@ -134,8 +134,8 @@ public class DataTest {
     }
 
     private Object testReadWriteWithoutClassCheck(Object originObj) {
-        Paper.book().write("obj", originObj);
-        Object readObj = Paper.book().read("obj");
+        Paper.put("obj", originObj);
+        Object readObj = Paper.get("obj");
         assertThat(readObj).isEqualTo(originObj);
         return readObj;
     }
